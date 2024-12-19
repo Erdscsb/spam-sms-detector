1b3ce1836578ff570895abe3dff4f43b63858890
erdscsb <erdos2.cs@gmail.com>
2024-12-19 12:42:38 +0100

    Update file path in training.py for improved portability

--- training.py
+++ training.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import pickle
 
 df = pd.read_csv(
-    "C:/Users/User/Downloads/Egyetem/Beadando/spam.csv", encoding="ISO-8859-1"
+    "spam.csv", encoding="ISO-8859-1"
 )
 df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1, inplace=True)
 df.rename(columns={"v1": "label", "v2": "message"}, inplace=True)
